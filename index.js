0385a679763348505c14ed28f3e38b0fe6962d79
Alvin Lim <alvink77@gmail.com>
2017-08-02 19:16:55 +0700

    init

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ import React from "react";
 import { render } from "react-dom";
 
 import Presentation from "./example/src";
+// import Presentation from "./react-tutorial/src"
 
 render(<Presentation/>, document.getElementById("root"));
